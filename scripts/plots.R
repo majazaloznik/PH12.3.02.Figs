bd711e70e8e9b24bca302cc71be3438ee1f5f9ec
majazaloznik <maja.zaloznik@gmail.com>
2016-07-22 14:51:38 +0100

    initial commit

---
+++ scripts/plots.R
@@ -0,0 +1,8 @@
+###############################################################################
+## Nice versions of figures for PopHzn 12(3) 
+## Asefa Hailemariam: The 1993 Population Policy of Ethiopia: how well did it work?
+###############################################################################
+## data copy pasted from submitted word document. double checked
+## sources were not checked but are given nonetheless
+###############################################################################
+
